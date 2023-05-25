f2ad7e50108b2096ceb00f71df37066a81c43169
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-05-24 22:32:41 -0400

    Change key name for jokes object

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react"
 import Joke from "./Joke"
-import jokesData from "./jokesData"
+import jokesData from "./JokesData"
 
 export default function App() {
     const jokeElements = jokesData.map(joke => {
-        return <Joke setup={joke.setup} punchline={joke.punchline} />
+        return <Joke setup={joke.opening} punchline={joke.punchline} />
     })
     return (
         <div>
--- src/jokesData.js
+++ src/jokesData.js
@@ -2,23 +2,23 @@
 
 export default [
     {
-        setup: "I got my daughter a fridge for her birthday.",
+        opening: "I got my daughter a fridge for her birthday.",
         punchline: "I can't wait to see her face light up when she opens it."
     },
     {
-        setup: "How did the hacker escape the police?",
+        opening: "How did the hacker escape the police?",
         punchline: "He just ransomware!"
     },
     {
-        setup: "Why don't pirates travel on mountain roads?",
+        opening: "Why don't pirates travel on mountain roads?",
         punchline: "Scurvy."
     },
     {
-        setup: "Why do bees stay in the hive in the winter?",
+        opening: "Why do bees stay in the hive in the winter?",
         punchline: "Swarm."
     },
     {
-        setup: "What's the best thing about Switzerland?",
+        opening: "What's the best thing about Switzerland?",
         punchline: "I don't know, but the flag is a big plus!"
     }
 ]
